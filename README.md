1b2343c77ea2e0b2bf8888f5c6cc29265b232c2b
balbonis <leyritana.jun.se@icloud.com>
2025-01-17 00:52:13 +0800

    initial

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # python_postgres_SQLAlchemy
+# Author: leyritana.jun.se@icloud.com
+# Date Created: 15-Jan-2025
+# Purpose: To test python script to test postgreSQL connection using SQLAlchemy
---
+++ python_postgres_SQLAlchemy.py
@@ -0,0 +1,48 @@
+from flask import Flask
+app = Flask(__name__)
+#####################DB connection option using SQLAlchemy#######################
+from sqlalchemy import create_engine, text
+
+# Define database connection parameters
+DB_USERNAME = "postgres"
+DB_PASSWORD = "Bongo6969"
+DB_HOST = "localhost" # or your database host
+DB_PORT = "5432" # default PostgreSQL port
+DB_NAME = "MyFlaskDB"
+
+
+# Create the database URL
+DATABASE_URL = f"postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+def connect_to_database_and_fetch_emails():
+    try:  
+        engine = create_engine(DATABASE_URL)
+        with engine.connect() as connection:
+            print ("Connection is successful! \n")      
+            global message 
+            global emailids
+            query = text("Select useremail from userslist;")
+            result = connection.execute(query)
+            for row in result:
+                print (row[0])
+                message = "Connected via SQLAlchemy"
+                emailids = str(row[0])
+                message = message + " " + "Created by: " + emailids
+
+        connection.close() 
+
+    except Exception as e:
+        print ("Unable to connect to Database \n")
+        print (f"Error: {e} \n")
+      
+    
+#####################DB connection option using SQLAlchemy#######################
+
+
+
+connect_to_database_and_fetch_emails()
+
+
+@app.route("/")
+def home():
+    return message
